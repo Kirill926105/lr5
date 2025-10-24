4aed75e4715646ceb20033781982ef65045c84a2
Кирилл <krzkerouzz@gmail.com>
2025-10-24 12:41:03 +0300

    commit'Доделал 4 задание'

--- task_4/main.py
+++ task_4/main.py
@@ -1,14 +1,14 @@
 print('Введите строки(текст): ') # kirill kulbitskiy, вводим строки(текст)
 lines = []
-while True: # 
+while True: # вводим строки, пока не будет пустой и потом выйдем
     line = input()
     if line == '':
         break
-    lines.append(line)
-print('\nРезультат:') # 
+    lines.append(line) # добавляем строку в список
+print('\nРезультат:') # выводим строку результат
 for line in lines:
     if len(line) > 1: # если длина введенного текста > 1, то меняем местами первую и последнюю буквы
        new_line = line[-1] + line[1:-1] + line[0]
-    else: #
+    else: # если строка короткая - не меняем
        new_line = line
-print('Измененный текст:', new_line) #
+print('Измененный текст:', new_line) # выводим измененный текст
